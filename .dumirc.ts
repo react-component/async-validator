5f5d284cdc09a065888de524c5930d0aa1ae9bc4
afc163 <afc163@gmail.com>
2023-10-16 15:51:53 +0800

    chore: fix doc deploy

--- .dumirc.ts
+++ .dumirc.ts
@@ -11,4 +11,5 @@ export default defineConfig({
     name: '@rc-component/async-validator',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
+  outputPath: '.docs-dist',
 });
